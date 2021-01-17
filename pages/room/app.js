dc9a22fb5d04d73584fabf6fd3b869d877924297
Erick Wendel <erick.workspace@gmail.com>
2021-01-17 20:39:35 -0300

    change layout
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- pages/room/app.js
+++ pages/room/app.js
@@ -8,20 +8,7 @@ const recordClick = function (recorderBtn) {
   }
 }
 
-const shareScreenClick = function (shareScreenBtn) {
-  this.shareScreenEnabled = false
-  return () => {
-    this.shareScreenEnabled = !this.shareScreenEnabled
-    const inactive = 'btn-success'
-    const active = 'btn-danger'
-    shareScreenBtn.classList.remove(active, inactive)
-    shareScreenBtn.classList.add(
-      this.shareScreenEnabled ? active : inactive
-    )
-  }
-}
-
-window.onload = () => {
+const onload = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const room = urlParams.get('room');
   console.log('this is the room', room)
@@ -29,7 +16,6 @@ window.onload = () => {
   const recorderBtn = document.getElementById('record')
   recorderBtn.addEventListener('click', recordClick(recorderBtn))
 
-  const shareScreenBtn = document.getElementById('shareScreen')
-  shareScreenBtn.addEventListener('click', shareScreenClick(shareScreenBtn))
-
 }
+
+window.onload = onload
